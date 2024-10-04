cf9534c2618f27d2f05f097b97f2ec61d2209a71
Vish Bhat <48611009+VishveshBhat8@users.noreply.github.com>
2024-10-04 15:14:14 -0700

    updating question generation algorithm with code that works

--- docs/generate_random_questions.py
+++ docs/generate_random_questions.py
@@ -1,34 +1,48 @@
 import random
-
-def get_random_questions(q):
-  N_00 = random.randint(1, 10)
-  N_01 = random.randint(1, 10)
-  N_02 = random.randint(1, 10)
-  N_03 = random.randint(1, 10)
-  N_04 = random.randint(1, 10)
-  N_05 = random.randint(1, 10)
-  N_06 = random.randint(1, 10)
-  N_07 = random.randint(1, 10)
-  N_08 = random.randint(1, 10)
-  N_09 = random.randint(1, 10)
-  N_10 = random.randint(1, 10)
-  N_11 = random.randint(1, 10)
-
-  formatted_question = q.format(N_00=N_00,
-                                N_01=N_01,
-                                N_02=N_02,
-                                N_03=N_03,
-                                N_04=N_04,
-                                N_05=N_05,
-                                N_06=N_06,
-                                N_07=N_07,
-                                N_08=N_08,
-                                N_09=N_09,
-                                N_10=N_10,
-                                N_11=N_11)
-
-  return formatted_question
+import yaml
 
 
-q = "James writes a {N_00}-page letter to {N_01} different friends twice a week.  How many pages does he write a year?"
-get_random_questions(q)
+
+def get_new_question_and_answer(data_entry):
+  keys = list(data_entry.keys())
+  variables = [key for key in keys if len(key) == 4 and "N_" in key]
+  formulae = [key for key in keys if len(key) != 4 and "N_" in key]
+  variables_with_formulae = [var for var in variables if any(var in formula for formula in formulae)]
+  variables_without_formulae = [var for var in variables if var not in variables_with_formulae]
+
+  variable_to_randomvalues = {}
+  formulae_to_substitutedvalues = {}
+
+  for var in variables_without_formulae:
+    variable_to_randomvalues[var] = random.randint(1, 50)
+
+  for formula in formulae:
+    try:
+      formula_with_values = data_entry[formula]
+      formula_var = formula.replace("_formula", "").strip()
+      for var in variable_to_randomvalues:
+        formula_with_values = formula_with_values.replace(var, str(variable_to_randomvalues[var]))
+
+      formulae_to_substitutedvalues[formula] = formula_with_values
+      final_value = eval(formula_with_values)
+
+      variable_to_randomvalues[formula_var] = final_value
+
+    except Exception as e:
+      print(e)
+      print(f"error calculating value for {formula}")
+    
+
+  combined = {**variable_to_randomvalues, **formulae_to_substitutedvalues}
+
+  new_question = data_entry['question_template'].format(**combined)
+  new_answer = data_entry['answer_template'].format(**combined)
+
+  return new_question, new_answer
+
+
+with open('/content/parameterized_gsm8k.yml', 'r') as file:
+    data = yaml.safe_load(file)
+
+for data_entry in data:
+  print(get_new_question_and_answer(data_entry))
